85379b99de297712820593547cf5b2039659d0a7
José Tarín <joseluis_tt88@hotmail.com>
2022-02-01 19:26:09 +1100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const { Cluster } = require("puppeteer-cluster")
 const cors = require('cors')
 const app = express()
 app.use(cors())
-let server = app.listen()
 
 app.get('/result', (req, res) => {
     (async () => {
@@ -61,7 +60,3 @@ app.get('/tides', (req, res) => {
 })
 
 app.listen(Port, () => console.log(`server running on PORT ${Port}`))
-server.on('clientError', (err, socket) => {
-    console.error(err);
-    socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
-  });
